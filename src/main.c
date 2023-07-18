297af93bd7a50c5198981f4033a3f53cbe1c6220
aosxap <aosxap@gmail.com>
2023-07-18 22:41:11 +0300

    restructure

---
+++ src/main.c
@@ -0,0 +1,49 @@
+#define MINIAUDIO_IMPLEMENTATION
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+
+#include "dependencies/miniaudio.h"
+#include "headers/queue.h"
+#include "headers/setup.h"
+
+void* task() {
+    while(true){
+        if(!MN_PLAYS() || is_init == false){
+            if(queue_top() != NULL){
+                //is_playing = true;
+                if(is_init == true) ma_sound_uninit(&sound);
+                PLAY(queue_top());
+            }else is_playing = false;
+        }
+        time_l++;
+        sleep(1);
+    }
+
+    pthread_exit(NULL);
+}
+
+
+int main(int argc, char **argv)
+{
+    INIT_MN();
+    int p_id = getpid();
+    printf("Process ID: %d\n", p_id);
+
+    pthread_t thread;
+    int ret = pthread_create(&thread, NULL, task, NULL);
+
+    while(true){
+        char command[1000], argument[1000];
+        printf("APC_ ");
+        scanf("%s", command);
+
+        mp_command(command);
+    }
+
+    UNINIT_MN();
+    return 0;
+}
