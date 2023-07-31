a26e6253242e12acc968a5ce6870b2952f1ec8e3
aosxap <aosxap@gmail.com>
2023-07-31 20:58:11 +0300

    change 323

--- make_scripts/init_miniaudio.sh
+++ make_scripts/init_miniaudio.sh
@@ -3,18 +3,12 @@ then
     rm -rf ./build_mini 
 fi
 
-if [ -d "bin" ] 
-then 
-    rm -rf ./bin
-fi
-
 if [ -f "libminiaudio.a" ]; then
     rm -f ./libminiaudio.a
 fi
 
 
 mkdir build_mini;
-mkdir bin;
 cd build_mini;
 cmake ../dependencies/miniaudio;
 make;
